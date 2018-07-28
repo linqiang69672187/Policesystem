1e51f531348b9eed42a05670a207a502ce30cbf1
LINQ <69672187@qq.com>
2018-07-28 22:07:50 +0800

    实时状况

---
+++ Policesystem/Policesystem/Handle/GetEntitys.ashx.cs
@@ -0,0 +1,52 @@
+﻿using DbComponent;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Policesystem.Handle
+{
+    /// <summary>
+    /// GetEntitys 的摘要说明
+    /// </summary>
+    public class GetEntitys : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            string search = context.Request.Form["search"];
+            string ssdd = context.Request.Form["ssdd"];
+            string sszd = context.Request.Form["sszd"];
+
+            StringBuilder sqltext = new StringBuilder();
+            
+          switch (ssdd)
+            {
+                case "":
+                case null://所有大队
+                    sqltext.Append("SELECT BMMC,BMDM,SJBM from [Entity] a where [SJBM]  = '331000000000' and BMMC like '台州市交通警察支队直属%' union all select BMMC,BMDM,SJBM from  [Entity] b where b.SJBM in (SELECT BMDM from [Entity]  where [SJBM]  = '331000000000' and BMMC like '台州市交通警察支队直属%')");
+                    break;
+
+                default:
+                    break;
+            }
+          
+
+
+            DataTable dt = SQLHelper.ExecuteRead(CommandType.Text, sqltext.ToString(), "DB");
+
+            context.Response.Write(JSON.DatatableToDatatableJS(dt, ""));
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
--- Policesystem/Policesystem/Js/map.js
+++ Policesystem/Policesystem/Js/map.js
@@ -1,5 +1,6 @@
-﻿$("#header").load('top.html', function () {
+﻿var entitydata;
 
+$("#header").load('top.html', function () {
 
 });
 $('.start_form_datetime').datetimepicker({
@@ -11,5 +12,57 @@ $('.start_form_datetime').datetimepicker({
 $(document).on('click.bs.carousel.data-api', '.boxleft > .row li > div', function (e) {
     $('.leftactive').removeClass();
     $(this).addClass('leftactive');
+    switch (e.target.id) {
+        case "ry": //人员
+            $("#deviceselect").val("0");
+            $(".seach-box input").attr('placeholder', '请输入姓名或警员编号');
+            break;
+        case "czsp": //车载视频
+            $("#deviceselect").val("1");
+            $(".seach-box input").attr('placeholder', '请输入车辆号码或设备编号');
+            break;
+        case "zfjly": //执法记录仪
+            $(".seach-box input").attr('placeholder', '请输入警员姓名或设备编号');
+            $("#deviceselect").val("5");
+            break;
+        case "fjt": //辅警通
+            $(".seach-box input").attr('placeholder', '请输入警员姓名或设备编号');
+            $("#deviceselect").val("6");
+            break;
+        case "jwt": //警务通
+            $(".seach-box input").attr('placeholder', '请输入警员姓名或设备编号');
+            $("#deviceselect").val("4");
+            break;
+        case "ljy": //拦截仪
+            $(".seach-box input").attr('placeholder', '请输入警员姓名或设备编号');
+            $("#deviceselect").val("3");
+            break;
+        case "djj": //对讲机
+            $(".seach-box input").attr('placeholder', '请输入警员姓名或设备编号');
+            $("#deviceselect").val("2");
+            break;
+        default:
+            break;
 
+    }
+});
+
+$.ajax({
+    type: "POST",
+    url: "../Handle/GetEntitys.ashx",
+    data: "",
+    dataType: "json",
+    success: function (data) {
+
+        entitydata = data; //保存单位数据
+        for (var i = 0; i < data.length; i++) {
+
+            if (data.SJBM == "331000000000") {  
+            $("#brigadeselect").append("<option value='" + data[i].BMDM + "'>" + data[i].Name + "</option>");
+            }
+        }
+    },
+    error: function (msg) {
+        console.debug("错误:ajax");
+    }
 });
