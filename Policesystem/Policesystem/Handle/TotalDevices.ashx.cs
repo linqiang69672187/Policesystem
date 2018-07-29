bb9f0819335a2b443c8ff65122403a9bdbdc9cfa
LINQ <69672187@qq.com>
2018-07-29 09:08:01 +0800

    大队

---
+++ Policesystem/Policesystem/Handle/TotalDevices.ashx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Web;
+using DbComponent;
+
+namespace Policesystem.Handle
+{
+    /// <summary>
+    /// 获取总设备数、总执法量、总在线数、总在线时长
+    /// </summary>
+    public class TotalDevices : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            StringBuilder sqltext = new StringBuilder();
+            //
+            sqltext.Append("SELECT count(ID) as value,0 as value2 FROM [Device] union all SELECT  sum(HandleCnt) as value,SUM([OnlineTime]) as value2  FROM StatsInfo_Yestorday_Today where Time > GETDATE()-1 union all SELECT  count(id) as value1,0 as value2 FROM [Gps] where IsOnline='1'");
+
+            DataTable dt = SQLHelper.ExecuteRead(CommandType.Text, sqltext.ToString(), "DB");
+
+            context.Response.Write(JSON.DatatableToDatatableJS(dt, ""));
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
--- Policesystem/Policesystem/Js/index.js
+++ Policesystem/Policesystem/Js/index.js
@@ -1,3 +1,27 @@
 ﻿$("#header").load('top.html', function () {
 
 });
+$(function () {
+    $.ajax({
+        type: "POST",
+        url: "Handle/TotalDevices.ashx",
+        data: "",
+        dataType: "json",
+        success: function (data) {
+            $(".qjxinxi label:eq(0)").text(data.data["0"].value);
+            $(".qjxinxi label:eq(2)").text(data.data["1"].value);
+            $(".qjxinxi label:eq(4)").text(data.data["2"].value);
+            $(".qjxinxi label:eq(6)").text(formatSeconds(data.data["1"].value2,1));
+        },
+        error: function (msg) {
+            console.debug("错误:ajax");
+        }
+    });
+
+
+});
+
+function formatSeconds(value,y) {
+    var result = Math.floor((value / 60 / 60) * Math.pow(10, y)) / Math.pow(10, y);
+    return result;
+}
