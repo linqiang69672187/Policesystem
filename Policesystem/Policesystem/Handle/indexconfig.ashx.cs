f8a8e10e978610dd438195ccfb95583e93914dc0
linqiang69672187 <69672187@qq.com>
2018-08-28 17:12:46 +0800

    首页参数

---
+++ Policesystem/Policesystem/Handle/indexconfig.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Policesystem.Handle
+{
+    /// <summary>
+    /// indexconfig 的摘要说明
+    /// </summary>
+    public class indexconfig : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+      
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
