4245d425ecc6a54b92d2b1b73619ffbd47e297f3
linqiang69672187 <69672187@qq.com>
2018-08-30 16:05:59 +0800

    首页大屏

--- Policesystem/Policesystem/Handle/index.ashx.cs
+++ Policesystem/Policesystem/Handle/index.ashx.cs
@@ -19,7 +19,7 @@ namespace Policesystem.Handle
             context.Response.ContentType = "text/plain";
             StringBuilder sqltext = new StringBuilder();
             //
-            sqltext.Append("SELECT  a.DevType,[Time],COUNT(a.id) as 设备数量,COUNT(u.id) as 人数,SUM(FileSize) as 文件大小,SUM(GFSCL) as 规范上传率,SUM(OnlineTime) as 在线总时长,SUM(HandleCnt) as 处理量,SUM(CXCNT) AS 查询量, sum((CASE WHEN([OnlineTime] +[HandleCnt]) > 0 THEN 1 ELSE 0 END)) 在线数 FROM [dbo].[StatsInfo_Yestorday_Today] a left JOIN Device d  on d.DevId = a.DevId   left JOIN ACL_USER  u  on u.JYBH = d.JYBH  where a.DevType in (1,2,3,4,5) GROUP BY a.DevType,TIME ORDER BY a.DevType, [Time]");
+            sqltext.Append("SELECT  a.DevType,[Time],COUNT(a.id) as 设备数量,COUNT(u.id) as 人数,SUM(FileSize) as 文件大小,SUM(GFSCL) as 规范上传率,SUM(OnlineTime) as 在线总时长,SUM(HandleCnt) as 处理量,SUM(CXCNT) AS 查询量, sum((CASE WHEN([OnlineTime] +[HandleCnt]) > 0 THEN 1 ELSE 0 END)) 在线数 FROM [dbo].[StatsInfo_Yestorday_Today] a left JOIN Device d  on d.DevId = a.DevId   left JOIN ACL_USER  u  on u.JYBH = d.JYBH  where a.DevType in (1,2,3,4,5,6) GROUP BY a.DevType,TIME ORDER BY a.DevType, [Time]");
 
             DataTable dt = SQLHelper.ExecuteRead(CommandType.Text, sqltext.ToString(), "DB");
 
