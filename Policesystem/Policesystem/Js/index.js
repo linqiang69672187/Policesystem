ac8b9826680893f75de64a531b60aaf95ac2748d
linqiang69672187 <69672187@qq.com>
2018-09-25 16:54:52 +0800

    更多

--- Policesystem/Policesystem/Js/index.js
+++ Policesystem/Policesystem/Js/index.js
@@ -1457,6 +1457,12 @@ $(document).on('click.bs.carousel.data-api', '.row2n,.row1n', function (e) {
 });
 
 $(document).on('click.bs.carousel.data-api', '.moreinfo', function (e) {
+    $("#infomodal ul").empty();
+    for (var i = 0; i < chartdata.length && i < 4; i++) {
+        $("#infomodal ul").append("<li><span class='glyphicon glyphicon-stop'><label class='ddlabel'>" + chartdata[i]['Name'] + "</label></span></li>")
+    }
+
+
     $("#infomodal").modal("show");
 
     return;
