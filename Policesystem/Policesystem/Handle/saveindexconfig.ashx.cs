7101c8831b35dec46e357a6f88e1abf6f3f0eef5
linqiang69672187 <69672187@qq.com>
2018-10-09 17:33:29 +0800

    数据库日志

--- Policesystem/Policesystem/Handle/saveindexconfig.ashx.cs
+++ Policesystem/Policesystem/Handle/saveindexconfig.ashx.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Web;
 
 namespace Policesystem.Handle
@@ -42,8 +44,58 @@ namespace Policesystem.Handle
             sp2[2] = new SqlParameter("@val", val3);
             SQLHelper.ExecuteNonQuery(CommandType.Text, "update IndexConfigs set DevType=@DevType,val=@val where id = @id", sp2);
 
+            try
+            {
+                string strHostName = Dns.GetHostName(); //得到本机的主机名 
+                IPHostEntry ipEntry = Dns.GetHostEntry(strHostName); //取得本机IP 
+                string IP = ipEntry.AddressList[1].ToString();
+
+                HttpCookie cookies = HttpContext.Current.Request.Cookies["cookieName"];
+                string JYBH = "331000000000";
+                if (cookies != null)
+                {
+                    JYBH = cookies["JYBH"];
+                }
+
+                string bz = "ID:1," + val1 + "ID:2" + val2 + "ID:3" + val3;
+
+                SqlParameter[] sp3 = new SqlParameter[7];
+                sp3[0] = new SqlParameter("@JYBH", JYBH);
+                sp3[1] = new SqlParameter("@Module", "07");
+                sp3[2] = new SqlParameter("@OperContent", "13");
+                sp3[3] = new SqlParameter("@IpAddr", IP);
+                sp3[4] = new SqlParameter("@LogTime", DateTime.Now.ToString());
+                sp3[5] = new SqlParameter("@BZ", bz);
+                sp3[6] = new SqlParameter("@OptObject", "参数设置");
+
+                SQLHelper.ExecuteNonQuery(CommandType.Text, "INSERT OperationLog (JYBH,Module,OperContent,IpAddr,LogTime,BZ,OptObject) VALUES (@JYBH,@Module,@OperContent,@IpAddr,@LogTime,@BZ,@OptObject)", sp3);
+
+            }
+            catch ( Exception ex)
+            {
+
+            }
+
+
+          
+
+
+
             context.Response.Write("1");
         }
+        private static string GetWebClientIp() {
+            var ip = GetWebRemoteIp();
+            foreach (var hostAddress in Dns.GetHostAddresses(ip))
+            {
+                if (hostAddress.AddressFamily == AddressFamily.InterNetwork) return hostAddress.ToString();
+            }
+            return string.Empty;
+        }
+        private static string GetWebRemoteIp() {
+            return HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"] ?? HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+        }
+
+
 
         public bool IsReusable
         {
